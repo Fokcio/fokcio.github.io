8eee6cbe775d9c667b393406df420459bf8d7901
Fokcio <tymoteusz.piotr.kwiatek@gmail.com>
2025-08-18 11:14:57 +0200

    Update indexkaruzela.js

--- js/indexkaruzela.js
+++ js/indexkaruzela.js
@@ -1,28 +1,43 @@
 // Karuzela obrazków
 const obrazki = [
-  { src: "images/gra1.png", href: "projects/PilkaNaRownowazni" },
-  { src: "images/gra2.png", href: "projects/Potatogame" },
-  { src: "images/Catus.png", href: "projects/catus" }
-  // Możesz dodać więcej:
-  // { src: "images/gra2.png", href: "/InnaGra" }
+  { 
+    src: "images/gra1.png", 
+    href: "projects/PilkaNaRownowazni",
+    opis: "Gra wykonana przez AI, polega na utrzymaniu piłki na platformie używając przycisków lub strzałek na klawiaturze."
+  },
+  { 
+    src: "images/gra2.png", 
+    href: "projects/Potatogame",
+    opis: "Zostań ziemniakiem, zbuduj świat, ukończ parkour, i się nie ugotuj"
+  },
+  { 
+    src: "images/Catus.png", 
+    href: "projects/catus",
+    opis: "<b>Catus</b> to fałszywy wirus komputerowy napisany w <mark>Pythonie</mark>,<br>gra muzykę i wyświetla <i>losowe KOTY</i>,<br>Aby wyłączyć należy kliknąć <b>ESC</b><br><small>Aplikacja została przetestowana na Windows 11</small>""
+  }
 ];
-
 
 let aktualnyObrazek = 0;
 const obrazekElement = document.getElementById("obrazek");
+const obrazekLink = document.getElementById("obrazekLink");
+const opisElement = document.getElementById("karuzelaOpis");
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 
 function pokazObrazek(index) {
   const obrazekDane = obrazki[index];
   obrazekElement.style.opacity = 0;
+  opisElement.style.opacity = 0;
+
   setTimeout(() => {
     obrazekElement.src = obrazekDane.src;
     obrazekLink.href = obrazekDane.href;
+    opisElement.innerHTML = obrazekDane.opis;
+    
     obrazekElement.style.opacity = 1;
+    opisElement.style.opacity = 1;
   }, 300);
 }
-
 
 prevButton.addEventListener("click", () => {
   aktualnyObrazek = (aktualnyObrazek - 1 + obrazki.length) % obrazki.length;
